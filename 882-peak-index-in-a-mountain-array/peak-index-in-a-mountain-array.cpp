6fac59e097b3105552d44311c62fc5577f135a06
NihalSingh2307M <nihalsingh2307n@gmail.com>
2026-03-05 14:37:20 +0530

    Time: 0 ms (100.00%) | Memory: 63.6 MB (48.09%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -4,7 +4,7 @@ public:
         int st =1;
         int end = arr.size()-2;
 
-        while(st<end){
+        while(st<=end){
             int mid = st+(end-st)/2;
             
             if(arr[mid-1]<arr[mid]&&arr[mid]>arr[mid+1]){
@@ -16,6 +16,6 @@ public:
                 end= mid-1;
             }
         }
-        return st;
+        return -1 ;
     }
 };

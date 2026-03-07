aa41e142a0b0f69e3f5ab95cb41326fe0f36ee5c
NihalSingh2307M <nihalsingh2307n@gmail.com>
2026-03-07 20:06:41 +0530

    Time: 32 ms (44.80%) | Memory: 14.5 MB (71.20%) - LeetSync

---
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int minFlips(string s) {
+        int n = s.length();
+
+        int result = INT_MAX;
+        int flip1 =0;
+        int flip2 = 0;
+
+        int i=0;
+        int j =0;
+
+        while(j<2*n){
+            char expecteds1 = (j%2) ? '1':'0';
+            char expecteds2 = (j%2) ? '0':'1';
+
+            if(s[j%n]!=expecteds1){
+                flip1++;
+            }
+            if(s[j%n]!=expecteds2){
+                flip2++;
+            }
+
+            if(j-i+1>n){//shrinking window (left)
+                expecteds1 = (i%2) ? '1':'0';
+                expecteds2 = (i%2) ? '0':'1';
+
+                 if(s[i%n]!=expecteds1){
+                flip1--;
+            }
+
+            if(s[i%n]!=expecteds2){
+                flip2--;
+            }
+            i++;
+
+            }
+             if(j-i+1==n){
+            result = min({result,flip1,flip2});
+        }
+        j++;
+
+        }
+       
+        return result;
+    }
+    
+};

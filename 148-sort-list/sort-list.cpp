ae973f538f8b97ced97d3ccc2fd3163c99fb8a87
NihalSingh2307M <nihalsingh2307n@gmail.com>
2026-08-30 20:25:47 +0530

    Time: 53 ms (19.84%) | Memory: 75.8 MB (20.00%) - LeetSync

---
+++ 148-sort-list/sort-list.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if(head==nullptr||head->next == nullptr){
+            return head;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast->next!= nullptr&& fast->next->next!= nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* second = slow->next;
+        slow->next =nullptr;
+
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(second);
+
+        ListNode* dummy = new ListNode(0);
+        ListNode* curr = dummy;
+
+        while(left!= nullptr&& right!=nullptr){
+            if(left->val<right->val){
+                curr->next = left;
+                left = left->next;
+            }else{
+                curr->next = right;
+                right = right->next;
+            }
+            curr= curr->next;
+        }
+
+        if(left!=nullptr){
+            curr->next = left;
+        }else{
+            curr->next = right;
+        }
+
+        return dummy->next;
+    }
+};

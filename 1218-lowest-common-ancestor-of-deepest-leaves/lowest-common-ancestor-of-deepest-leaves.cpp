ba0418df05e5b6998d982c51c70dc9521cfaf88c
NihalSingh2307M <nihalsingh2307n@gmail.com>
2026-09-07 22:27:57 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (30.40%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -1,36 +1,44 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-
-    pair<int, TreeNode*> dfs(TreeNode* root) {
-        if (root == nullptr)
-            return {0, nullptr};
-
-        auto left = dfs(root->left);
-        auto right = dfs(root->right);
-
-        if (left.first > right.first) {
-            return {left.first + 1, left.second};
-        }
-
-        if (right.first > left.first) {
-            return {right.first + 1, right.second};
-        }
-
-        return {left.first + 1, root};
-    }
-
-    TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        return dfs(root).second;
-    }
+class Solution { 
+public: 
+    int maxDepth = 0; 
+    TreeNode* ans = nullptr; 
+    TreeNode* treeRoot = nullptr;
+ 
+    void dfs(TreeNode* root, int depth) { 
+        if (root == nullptr) 
+            return; 
+ 
+        if (depth > maxDepth) { 
+            maxDepth = depth; 
+            ans = root; 
+        } 
+        else if (depth == maxDepth) { 
+            ans = lca(treeRoot, ans,root); 
+        } 
+ 
+        dfs(root->left, depth + 1); 
+        dfs(root->right, depth + 1); 
+    } 
+ 
+    TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q) { 
+        if (root == nullptr || root == p || root == q) 
+            return root; 
+ 
+        TreeNode* left = lca(root->left, p, q); 
+        TreeNode* right = lca(root->right, p, q); 
+ 
+        if (left == nullptr) 
+            return right; 
+ 
+        if (right == nullptr) 
+            return left; 
+ 
+        return root; 
+    } 
+ 
+    TreeNode* lcaDeepestLeaves(TreeNode* root) { 
+        treeRoot = root;
+        dfs(root, 0); 
+        return ans; 
+    } 
 };
